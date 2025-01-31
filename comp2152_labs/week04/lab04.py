1e3b610edbd5277301940e2e363e7c13e7c4a204
M Shaffaq Hai <hai.shaffaq@gmail.com>
2025-01-31 13:47:57 -0500

    adding q1

--- comp2152_labs/week04/lab04.py
+++ comp2152_labs/week04/lab04.py
@@ -81,6 +81,14 @@ loot_options = ["Health Potion", "Poison Potion", "Secret Note", "Leather Boots"
 good_loot_options = ["Health Potion", "Leather Boots"]
 bad_loot_options = ["Poison Potion"]
 
+#lab4 q1 - defining the monster power:
+monster_power = {
+    "Fire Magic": 2,
+    "Freezing Time": 4,
+    "Super Hearing": 6,
+}
+
+
 # Define the number of stars awarded to the Player
 num_stars = 0
 
