fa0df5a441fc1193c2b58b522b53bfcd1e9bf2aa
M Shaffaq Hai <hai.shaffaq@gmail.com>
2025-02-07 13:48:06 -0500

    added q4

--- comp2152_labs/week05/functions_lab05.py
+++ comp2152_labs/week05/functions_lab05.py
@@ -6,19 +6,20 @@ import random
 
 
 # Lab 5: Question 4
-def use_loot():
+def use_loot(belt,health_points):
     good_loot_options = ["Health Potion", "Leather Boots"]
     bad_loot_options = ["Poison Potion"]
 
     print("    |    !!You see a monster in the distance! So you quickly use your first item:")
-    
-    if :
-        print("    |    You used " +  +" to up your health to " )
-    elif :
+    first_item = belt.pop(0)
+    if first_item in good_loot_options:
+        health_points = min(20,(health_points + 2))
+        print("    |    You used " + first_item  +" to up your health to " + str(health_points) + " points." )
+    elif  first_item in bad_loot_options:
         
-        print("    |    You used " +  + " to hurt your health to " + )
+        print("    |    You used " + first_item  + " to hurt your health to " +  str(health_points) +" points." )
     else:
-        print("    |    You used " +  + " but it's not helpful")
+        print("    |    You used " + first_item + " but it's not helpful")
     return 
 
 
@@ -40,7 +41,10 @@ def collect_loot(loot_options, belt):
               @@@@@@@@@@@@          
               """
     print(ascii_image3)
-
+    loot_roll = random.choice(range(1,len(loot_options)+1))
+    loot = loot_options.pop(loot_roll -1)
+    belt.append(loot)
+    return loot_options,belt
 
 
 # Hero's Attack Function
