0d5394000650b496b5f137e657948947e6e67887
M Shaffaq Hai <hai.shaffaq@gmail.com>
2025-02-07 13:37:51 -0500

    added q1

--- comp2152_labs/week05/lab05.py
+++ comp2152_labs/week05/lab05.py
@@ -226,20 +226,23 @@ if not input_invalid:
     while input_invalid and tries in range(5):
         print("    |", end="    ")
 
-        # Lab 5 - Question 1: 
+        # Lab 5 - Question 1:
+
         hero_name = input("Enter your Hero's name (in two words)")
+        name = hero_name.split()
         
-        if :
+        if len(name) != 2 :
             print("    |    Please enter a name with two parts (separated by a space)")
-            
+            tries += 1
         else:
-            if :
+            if not name[0].isalpha() or not name[1].isalpha():
                 print("    |    Please enter an alphabetical name")
+                tries += 1
                 
             else:
-                
+                short_name = name[0][0:2:1] + name[1][0:1:1]
                 print("    |    I'm going to call you " + short_name + " for short")
-                
+                input_invalid = False
 
     if not input_invalid:
         stars_display = "*" * num_stars
