462fb9d740dcc6eb50aa51d5ae3f08ea98eaf780
Shaffaq Hai <hai.shaffaq@gmail.com>
2025-03-24 15:07:07 -0400

    made charcter private

--- comp2152_assignments/assignment2/character.py
+++ comp2152_assignments/assignment2/character.py
@@ -4,8 +4,8 @@ class Character:
 
     def __init__(self):
 
-        self._combat_strength = random.randrange(1, 7)
-        self._health_points = random.randrange(1, 7)
+        self.__combat_strength = random.randrange(1, 7)
+        self.__health_points = random.randrange(1, 7)
 
     def __del__(self):
          print("The character is destroyed")
