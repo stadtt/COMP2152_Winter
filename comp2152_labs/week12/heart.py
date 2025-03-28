84dab843e1afb7800bdbc7df69a1b7711a8ff8f7
M Shaffaq Hai <hai.shaffaq@gmail.com>
2025-03-28 13:21:13 -0400

    created heart class

--- comp2152_labs/week12/heart.py
+++ comp2152_labs/week12/heart.py
@@ -1,3 +1,13 @@
 # Heart class for composition
 class Heart:
-    pass
+   def __init__(self):
+       print("Composition: Heart is created.")
+       self.beats_per_minutes = 72
+
+   def beat(self):
+       print("Heart is beating.")
+
+   def __del__(self):
+       print("Composition: Heart is destroyed")
+
+
